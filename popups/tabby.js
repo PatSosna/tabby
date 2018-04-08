4790ac3106a0429137040e091e5183bb96a45cd6
DanCharousek <dancharousek@gmail.com>
2018-04-08 11:10:28 +0200

    Add basic event handling

--- popups/tabby.js
+++ popups/tabby.js
@@ -1,1 +1,40 @@
 const wrapper = document.getElementById('tabby');
+
+const tabby = {
+
+    storeOpenedTabs(tabs) {
+        const key = Object.keys(tabs[0]);
+        const contentToStore = [];
+
+        tabs.map((tab, index) => {
+            contentToStore.push({
+                url: tab.url,
+                title: tab.title,
+                favicon: tab.favIconUrl
+            });
+        });
+
+        // TODO: Prompt user for group name
+    },
+
+    open() {
+        // TODO: Offer stored group to be opened
+    },
+
+    saveAs() {
+        browser.tabs.query({ currentWindow: true })
+            .then(this.storeOpenedTabs);
+    }
+};
+
+document.addEventListener('click', e => {
+    const event = e.target.getAttribute('data-event');
+    switch (event) {
+        case 'save-as':
+            tabby.saveAs();
+            break;
+        case 'open':
+            tabby.open();
+            break;
+    }
+});
