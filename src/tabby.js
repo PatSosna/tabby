f0dcb40488a6f121b58aa11d2020a2c6739dd482
DanCharousek <DanCharousek@gmail.com>
2018-08-04 10:09:50 +0200

    Close popup after it loses focus

--- src/tabby.js
+++ src/tabby.js
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './components/app';
 import store from './state';
 
+// Close popup when loses focus
+window.addEventListener('blur', () => { window.close() });
+
 const app = new Vue({
     el: '#tabby',
     store,
